abe80588ecbf60c4fe96092c08ce29f64fec6ea5
valgoun <valgoun@gmail.com>
2017-01-20 23:41:35 +0100

    Base system

---
+++ Assets/Scritps/Character/Character.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public enum WaveShape
+{
+    Sin,
+    Linear,
+    Square
+}
+public class Character : MonoBehaviour
+{
+    public float Speed = 6f;
+    public float Bpm = 60;
+    public float InputWindow = 0.5f;
+    public float DoubleInputPrecision = 0.2f;
+    public AnimationCurve SquareCurve;
+
+    private SpriteRenderer _spriteRenderer;
+    private bool _inputSensible = true;
+    private float _timeBeforeNextWindow;
+    private WaveShape _nextShape;
+    private Tween _movement;
+    private bool _shouldCreate = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _timeBeforeNextWindow = 30f / Bpm;
+        _nextShape = WaveShape.Linear;
+        _movement = transform.DOMoveY(3, _timeBeforeNextWindow / 2).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo).OnComplete(() => CreateTween());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.y) < (3 * InputWindow) / _timeBeforeNextWindow)
+        {
+            _spriteRenderer.color = Color.green;
+
+            //frequence inputs
+            if (Input.GetButtonDown("Croche"))
+            {
+                // Debug.Log("Croche");
+                _timeBeforeNextWindow = 30f / Bpm;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+            else if (Input.GetButtonDown("Noire"))
+            {
+                // Debug.Log("Noire");
+                _timeBeforeNextWindow = 60f / Bpm;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+            else if (Input.GetButtonDown("Triolet"))
+            {
+                // Debug.Log("Triolet");
+                _timeBeforeNextWindow = 90f / Bpm;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+            else if (Input.GetButtonDown("Blanche"))
+            {
+                // Debug.Log("Blanche");
+                _timeBeforeNextWindow = 120f / Bpm;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+
+            //Shape Input
+            else if (Input.GetButtonDown("Sin"))
+            {
+                Debug.Log("Sin");
+                _nextShape = WaveShape.Sin;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+            else if (Input.GetButtonDown("Linear"))
+            {
+                Debug.Log("Linear");
+                _nextShape = WaveShape.Linear;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+            else if (Input.GetButtonDown("Square"))
+            {
+                Debug.Log("Square");
+                _nextShape = WaveShape.Square;
+                if (!_shouldCreate)
+                {
+                    _shouldCreate = true;
+                    DOVirtual.DelayedCall(DoubleInputPrecision, () => CreateTween());
+                }
+            }
+        }
+        else
+            _spriteRenderer.color = Color.white;
+
+    }
+
+    private void CreateTween()
+    {
+        if (_shouldCreate)
+        {
+            switch (_nextShape)
+            {
+                case WaveShape.Linear:
+                    _movement = transform.DOMoveY(3, _timeBeforeNextWindow / 2).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo).OnComplete(() => CreateTween());
+                    break;
+                case WaveShape.Sin:
+                    _movement = transform.DOMoveY(3, _timeBeforeNextWindow / 2).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo).OnComplete(() => CreateTween());
+                    break;
+                case WaveShape.Square:
+                    _movement = transform.DOMoveY(3, _timeBeforeNextWindow / 2).SetEase(SquareCurve).SetLoops(2, LoopType.Yoyo).OnComplete(() => CreateTween());
+                    break;
+                default:
+                    break;
+            }
+            _shouldCreate = false;
+        }
+    }
+}
