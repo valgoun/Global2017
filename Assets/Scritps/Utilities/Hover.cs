b3c5e50072458a78c00f464ada4d0a8663a5bc51
valgoun <valgoun@gmail.com>
2017-01-21 15:12:54 +0100

    Sprite integration

---
+++ Assets/Scritps/Utilities/Hover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Hover : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.DOLocalMoveY(0.2f, 0.4f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
