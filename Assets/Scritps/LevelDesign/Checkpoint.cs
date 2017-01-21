acf2af879c38dbcd768cb6650a95f2d1ce118b7f
valgoun <valgoun@gmail.com>
2017-01-21 17:06:39 +0100

    Fix trajectory

---
+++ Assets/Scritps/LevelDesign/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public WaveShape Shape;
+    public Rythm Note;
+
+    // Use this for initialization
+    void Start()
+    {
+        CheckpointsManager.Instance.addCheckPoint(this);
+    }
+}
---
+++ Assets/Scritps/Managers/CheckpointsManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointsManager : MonoBehaviour
+{
+    public static CheckpointsManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private static CheckpointsManager _instance;
+    private List<Checkpoint> _points;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.LogError("there already is a MaskManager");
+            Destroy(this.gameObject);
+        }
+        else
+            _instance = this;
+
+    }
+    public void addCheckPoint(Checkpoint cp)
+    {
+        if (_points == null)
+        {
+            _points = new List<Checkpoint>();
+            _points.Add(cp);
+            return;
+        }
+        for (int i = 0; i < _points.Count; i++)
+        {
+            var p = _points[i];
+            if (cp.transform.position.x < p.transform.position.x)
+            {
+                _points.Insert(i, cp);
+                break;
+            }
+        }
+    }
+
+    public Checkpoint Pop()
+    {
+        Checkpoint p;
+        if (_points.Count != 0)
+            p = _points[0];
+        else
+            return null;
+        _points.Remove(p);
+        return p;
+    }
+}
